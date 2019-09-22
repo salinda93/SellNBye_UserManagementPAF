72db4f6f5c47b0762154800a9dc0a0305623b30f
Salinda <salinda.jayasundara3010@gmail,com>
2019-09-22 22:13:13 +0530

    initial commit

--- src/main/java/com/PAF_UserManagementSellNBye/controller/CustomerController.java
+++ src/main/java/com/PAF_UserManagementSellNBye/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.PAF_UserManagementSellNBye.dao.CustomerDAO;
@@ -21,76 +22,89 @@ import com.PAF_UserManagementSellNBye.model.Customer;
 @RestController
 @RequestMapping("/SellNBye")
 public class CustomerController {
-	
+
 	@Autowired
 	CustomerDAO customerDAO;
+
 	
-	//to save a customer 
-	
-	@PostMapping("/customers") 
-	public Customer createCustomer(@Valid @RequestBody Customer cus1) {
-		return customerDAO.save(cus1);
+	Customer customer =new Customer();
+
+	// to save a customer
+
+	@PostMapping("/customers")
+	public Customer createCustomer(@Valid @RequestParam(name = "name") String cus_Name,
+			@RequestParam(name = "address") String cus_Address, @RequestParam(name = "email") String cus_Email,
+			@RequestParam(name = "phoneNo") String cus_PhoneNo, @RequestParam(name = "country") String cus_Country) {
+
+		customer.setName(cus_Name);
+		customer.setAddress(cus_Address);
+		customer.setEmail(cus_Email);
+		customer.setPhoneNo(cus_PhoneNo);
+		customer.setCountry(cus_Country);
+
+		return customerDAO.save(customer);
 	}
-	
+
 	// get all customer
-	
+
 	@GetMapping("/customers")
-	public List<Customer> getAllCustomers(){
+	public List<Customer> getAllCustomers() {
+		System.out.println("Request recived for all customers");
 		return customerDAO.findAll();
 	}
-	
-	//get customer by cus1id
-	
+
+	// get customer by cus1id
+
 	@GetMapping("/customers/{id}")
-	public ResponseEntity<Customer> getCustomerById(@PathVariable(value = "id") Long cus1id){
-		
-		 Customer cus1 = customerDAO.findOne(cus1id);
-		 
-		 if(cus1 == null) {
-			 return ResponseEntity.notFound().build();
-		 }
-		 return ResponseEntity.ok().body(cus1);
+	public ResponseEntity<Customer> getCustomerById(@PathVariable(value = "id") Long cus1id) {
+
+		Customer cus1 = customerDAO.findOne(cus1id);
+
+		if (cus1 == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().body(cus1);
 	}
-	
-	//update customer by cus1id
-	
+
+	// update customer by cus1id
+
 	@PutMapping("/customers/{id}")
-	public ResponseEntity<Customer> updateCustomer(@PathVariable(value = "id") Long cus1id,@Valid @RequestBody Customer cus1Details){
-		
+	public ResponseEntity<Customer> updateCustomer(@PathVariable(value = "id") Long cus1id,
+			@Valid @RequestBody Customer cus1Details) {
+
 		Customer cus1 = customerDAO.findOne(cus1id);
-		
-		 if(cus1 == null) {
-			 return ResponseEntity.notFound().build();
-			 
-		 }
-		 
-		 cus1.setName(cus1Details.getName());
-		 cus1.setAddress(cus1Details.getAddress());
-		 cus1.setEmail(cus1Details.getEmail());
-		 cus1.setPhoneNo(cus1Details.getPhoneNo());
-		 
-		 Customer updateCustomer = customerDAO.save(cus1);
-		 
-		 return ResponseEntity.ok().body(updateCustomer);
+
+		if (cus1 == null) {
+			return ResponseEntity.notFound().build();
+
+		}
+
+		cus1.setName(cus1Details.getName());
+		cus1.setAddress(cus1Details.getAddress());
+		cus1.setEmail(cus1Details.getEmail());
+		cus1.setPhoneNo(cus1Details.getPhoneNo());
+
+		Customer updateCustomer = customerDAO.save(cus1);
+
+		return ResponseEntity.ok().body(updateCustomer);
 	}
-	
-	//delete a customer
-	
+
+	// delete a customer
+
 	@DeleteMapping("/customers/{id}")
-	public ResponseEntity<Customer>deletEmployee(@PathVariable(value = "id") Long cus1id){
-		
+	public ResponseEntity<Customer> deletEmployee(@PathVariable(value = "id") Long cus1id) {
+
 		Customer cus1 = customerDAO.findOne(cus1id);
-		
-		if(cus1 == null) {
-			 return ResponseEntity.notFound().build();
-			 
-		 }
-		
+
+		if (cus1 == null) {
+			return ResponseEntity.notFound().build();
+
+		}
+
 		customerDAO.delete(cus1);
-		
+
 		return ResponseEntity.ok().build();
-		 
-		
+
 	}
 
 }
